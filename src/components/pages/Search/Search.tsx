46e857ded1eec8335d587769f590e5a83f705559
DIEN94 <you@example.comdm.kovalchyk94@gmail.com>
2023-08-10 15:37:14 +0300

    add pages

---
+++ src/components/pages/Search/Search.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import classes from "./Search.module.scss"
+
+export const Search: FC = () => {
+  return (
+    <div className={classes.search}>
+        SEARCH
+    </div>
+  )
+}
