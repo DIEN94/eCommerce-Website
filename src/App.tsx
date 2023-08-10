d406f0dc4f5f3cdd4f3ec78b8fd908daf5bf7fdc
DIEN94 <dm.kovalchyk94@gmail.com>
2023-08-10 14:00:50 +0300

    add Layout/ correction header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, {} from "react";
 import './styles/App.scss'
 import { Header } from './components/Header/Header';
+import Layout from "components/Layout/Layout";
 
 export function App() {
 
   return (
     <div className="myApp">
-       <Header/>
+      <Layout>
+        <Header/>
+      </Layout>
     </div>
   );
 }
