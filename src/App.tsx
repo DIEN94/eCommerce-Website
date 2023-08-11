60e29269b27ea8fc481c332a3a95ad433922a290
DIEN94 <you@example.comdm.kovalchyk94@gmail.com>
2023-08-11 15:18:53 +0300

    create router

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React, {} from "react";
 import './styles/App.scss'
 import { Header } from './components/Header/Header';
 import Layout from "components/Layout/Layout";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "components/AppRouter/AppRouter";
 
 export function App() {
 
   return (
     <div className="myApp">
+    <BrowserRouter>
       <Layout>
         <Header/>
+        <AppRoutes/>
       </Layout>
+    </BrowserRouter>
     </div>
   );
 }
