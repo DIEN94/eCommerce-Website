e7be74287b17de234eaa848f147219dcc0876bb0
DIEN94 <dm.kovalchyk94@gmail.com>
2023-08-12 12:49:20 +0300

    create NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,12 +1,35 @@
 import React, {FC, useState} from 'react'
 import classes from "./NotFoundPage.module.scss"
-
+import { Link } from 'react-router-dom'
+import { MyButton } from 'components/Button/MyButton'
 
 export const NotFoundPage: FC = () => {
 
+    const [isHighlighted, setIsHighlighted] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHighlighted(true);
+    };
+  
+    const handleMouseLeave = () => {
+        setIsHighlighted(false);
+    };
+
   return (
     <div className={classes.notFoundPage}>
-    NOT FOUND PAGE
+      <h1>Page Not Found</h1>
+      <p>The requested page could not be found.</p>
+      <Link 
+      to="/" 
+      className={`${classes.link} ${isHighlighted ? classes.show : ""}`}
+          onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
+            <MyButton
+              variant="no-fill"
+              className={`${classes.button} ${isHighlighted ? classes.show : ""}`}>
+              Return to Home Page
+            </MyButton>
+      </Link>
     </div>
   )
 }
