39cbab903fea757b15a53bcf488f02d7c9876814
DIEN94 <dm.kovalchyk94@gmail.com>
2023-08-13 19:44:58 +0300

    page-Home/create first section (poster)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,36 @@
 import React, { FC } from 'react'
+import homePoster from "assets/page-Home/home-poster.webp";
 import classes from "./Home.module.scss"
+import { MyButton } from 'components';
+import { Link } from 'react-router-dom';
 
 export const Home: FC = () => {
   return (
     <div className={classes.home}>
-        HOME
+      <div className={classes.poster}>
+        <img src={homePoster}/>
+        <div className={classes.posterStick}>
+          <p className={classes.firstLevelText}>
+            New Arrival
+          </p>
+          <h2 className={classes.secondLevelText}>
+            Discover Our <br/>
+            New Collection
+          </h2>
+          <p className={classes.thirdLevelText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut <br/>
+            elit tellus, luctus nec ullamcorper mattis.
+          </p>
+          <Link to={"/shop"} style={{ textDecoration: 'none' }}>
+            <MyButton 
+            className={classes.button} 
+            variant='fill-orange'>
+              BUY NOW
+            </MyButton>
+          </Link>
+
+        </div>
+      </div>
     </div>
   )
 }
