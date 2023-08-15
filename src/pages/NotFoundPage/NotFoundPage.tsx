33f60e66ae8c3c714ae8f4f3e0ea8fc80b55a059
DIEN94 <you@example.comdm.kovalchyk94@gmail.com>
2023-08-15 17:25:32 +0300

    page-Not Found Pag/correction styles (add hover)

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -5,28 +5,16 @@ import { MyButton } from 'components/Button/MyButton'
 
 export const NotFoundPage: FC = () => {
 
-    const [isHighlighted, setIsHighlighted] = useState(false);
-
-    const handleMouseEnter = () => {
-        setIsHighlighted(true);
-    };
-  
-    const handleMouseLeave = () => {
-        setIsHighlighted(false);
-    };
-
   return (
     <div className={classes.notFoundPage}>
       <h1>Page Not Found</h1>
       <p>The requested page could not be found.</p>
       <Link 
       to="/" 
-      className={`${classes.link} ${isHighlighted ? classes.show : ""}`}
-          onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}>
+      className={classes.link}>
             <MyButton
               variant="no-fill"
-              className={`${classes.button} ${isHighlighted ? classes.show : ""}`}>
+              className={classes.button}>
               Return to Home Page
             </MyButton>
       </Link>
