01ae5ce4ceaabae04c739a66f4795bc6706702c5
DIEN94 <dm.kovalchyk94@gmail.com>
2023-09-09 14:32:25 +0300

    reusable-components / correction MyModal

--- src/components/Modal/MyModal.tsx
+++ src/components/Modal/MyModal.tsx
@@ -3,53 +3,69 @@ import { useClickOutside } from "../../hooks/useClickOutside";
 import ModalPortal from "./components/ModalPortal/ModalPortal";
 import classes from "./MyModal.module.scss";
 
-
 export interface IModal {
   children: React.ReactNode;
   isOpen: boolean;
   onClose: () => void;
   className?: string;
+  animateClasses?:{
+    show?:string;
+    hide?:string;
+  }
 }
 
-export const MyModal: FC<IModal> = ({ children, isOpen, onClose, className }) => {
+export const MyModal: FC<IModal> = ({
+  children,
+  isOpen,
+  onClose,
+  className,
+  animateClasses
+}) => {
   const [open, setOpen] = useState(isOpen);
   const ref = useRef<HTMLDivElement>(null);
-  const rootClasses = [classes.myModal];
+  const rootClasses:Array<string|undefined> = [classes.myModal];
 
   if (open) {
-    rootClasses.push(classes.active);
+    rootClasses.push(classes.active,);
   }
-  if (!isOpen) {
-    rootClasses.push(classes.hide);
+  if (!open) {
+    rootClasses.push(classes.hide,);
   }
 
-  useClickOutside(ref, onClose)
+  useClickOutside(ref, onClose);
 
-    useEffect(() => {
-      if (isOpen) {
-        setOpen(isOpen);
-      }
-    }, [isOpen]);
+  useEffect(() => {
+      setOpen(isOpen);
+  }, [isOpen]);
 
-    useEffect(() => {
-      document.body.style.overflow = open ? "hidden" : "auto";
-      return () => {
-        document.body.style.overflow = "auto";
-      };
-    }, [open]);
+  useEffect(() => {
+    if(!ref.current) return
+    if (open) {
+      setTimeout(()=>{
+      if(!ref.current) return
+      ref.current.classList.remove(animateClasses?.hide || "")
+      ref.current.classList.add(animateClasses?.show || "")}, 100)
+    }else{
+      ref.current.classList.remove(animateClasses?.show || "")
+      ref.current.classList.add(animateClasses?.hide || "")
+    }
+  }, [open]);
+
+  useEffect(() => {
+    document.body.style.overflow = open ? "hidden" : "auto";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [open]);
 
   return (
     <ModalPortal>
-    <div
-      onTransitionEnd={() => {setOpen(false); onClose();}}
-      className={rootClasses.join(" ")}
-      >
-      <div className = {className} ref={ref}>
-        {children} 
+      <div className={rootClasses.join(" ")}>
+      <div className={`${classes.modalContent} ${className}`} ref={ref}>
+          {children}
+        </div>
       </div>
-    </div>
     </ModalPortal>
-
   );
 };
 
