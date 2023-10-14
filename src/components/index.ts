34261a218b607bd6055ea1bdda494a09ad0ce140
DIEN94 <dm.kovalchyk94@gmail.com>
2023-10-14 08:55:59 +0300

    reusable-components / create PosterPage component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,9 @@
-
 import ModalPortal from './Modal/components/ModalPortal/ModalPortal';
-
 
 export {MyButton} from './Button/MyButton';
 
 export {CardOfProduct} from './CardOfProduct/CardOfProduct';
+// export {CardOfProductWide} from './CardOfProductWide/CardOfProductWide';
 export {Hover} from './CardOfProduct/components/Hover/Hover';
 
 export {DropDown} from './DropDown/DropDown';
@@ -12,4 +11,6 @@ export {DropDown} from './DropDown/DropDown';
 export {MyInput} from './Input/MyInput';
 
 export {MyModal} from './Modal/MyModal';
-export {ModalPortal} 
+export {ModalPortal}
+
+export {PosterPage} from './PosterPage/PosterPage';
