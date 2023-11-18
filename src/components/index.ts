83c1633f7f37328081dde8d41bae8e79258f39ad
DIEN94 <dm.kovalchyk94@gmail.com>
2023-11-18 15:18:23 +0300

    develop / correction index components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,16 @@
 export {MyButton} from './Button/MyButton';
-
 export {CardOfProduct} from './CardOfProduct/CardOfProduct';
 export {CardOfProductWide} from './CardOfProductWide/CardOfProductWide';
 export {Hover} from './CardOfProduct/components/Hover/Hover';
-
 export {DropDown} from './DropDown/DropDown';
-
+export {Footer} from './Footer/Footer';
+export {FormField} from './FormField/FormField';
+export {Header} from './Header/Header';
 export {InformationBoard} from './InformationBoard/InformationBoard';
-
 export {MyInput} from './Input/MyInput';
-
+export {default as Layout} from './Layout/Layout';
 export {MyModal} from './Modal/MyModal';
 export {default as ModalPortal} from './Modal/components/ModalPortal/ModalPortal';
-
 export {PosterPage} from './PosterPage/PosterPage';
+export {Providers} from './Providers/Providers';
+export {Slider} from './Slider/Slider';
