ee68980b2d2fedef657e9992c282f29ab219c606
DIEN94 <dm.kovalchyk94@gmail.com>
2023-11-18 12:05:12 +0300

    page-Contact / create page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,10 +1,31 @@
 import React, { FC } from 'react'
+import { InformationBoard, PosterPage } from 'components'
+import { contactPageConfig } from './config'
 import classes from "./Contact.module.scss"
 
 export const Contact: FC = () => {
   return (
     <div className={classes.contact}>
-        CONTACT
+        <PosterPage namePage='Contact'/>
+        <div className={classes.contactContainer}>
+          {contactPageConfig.map(({src, name, description}, index)=> {
+                  return (
+                      <div
+                      className={classes.element}
+                      key={index}>
+                          <div className={classes.imgContainer}>
+                            <img src={src} alt={`${src}`}/>
+                          </div>
+                          <div
+                          className={classes.textContainer}>
+                          <p className={classes.nameText}>{name}</p>
+                          <p className={classes.descriptionText}>{description}</p>
+                          </div>
+                      </div>
+                  );
+          })}
+        </div>
+        <InformationBoard/>
     </div>
   )
 }
