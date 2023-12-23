ad9e06c4b9edccf49b9f727d38fcb5055dd3f646
DIEN94 <dm.kovalchyk94@gmail.com>
2023-12-23 13:57:09 +0200

    redux / correction cart reducer & recreate modalOpen reducer

--- src/store/reducers/modalOpen/index.ts
+++ src/store/reducers/modalOpen/index.ts
@@ -1,22 +1,52 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-interface OpenModalState {
+interface modalProps {
+  id: string;
   isOpen: boolean;
 }
 
-const initialState:OpenModalState = {
-    isOpen: false
+interface ModalState {
+  modals: modalProps[];
 }
+
+  const getInitialModalState = () => {
+    const storedState = localStorage.getItem("modalsState");
+    const initialState: ModalState = storedState
+      ? JSON.parse(storedState)
+      : { modals: [] };
+    return initialState;
+  };
+
+const initialState:ModalState = getInitialModalState()
 
 export const openModalSlice = createSlice({
     name: 'modalOpen',
     initialState,
     reducers: {
-        setOpen(state, action: PayloadAction<boolean>) {
-          state.isOpen = action.payload;
-        },
+      openModal(state, action: PayloadAction<string>) {
+        const modalId = action.payload;
+        const existingModal = state.modals.find((modal) => modal.id === modalId);
+  
+        if (existingModal) {
+          existingModal.isOpen = true;
+        } else {
+          state.modals.push({ id: modalId, isOpen: true });
+        }
+  
+        localStorage.setItem("modalsState", JSON.stringify(state));
       },
+      closeModal(state, action: PayloadAction<string>) {
+        const modalId = action.payload;
+        const existingModal = state.modals.find((modal) => modal.id === modalId);
+  
+        if (existingModal) {
+          existingModal.isOpen = false;
+        }
+  
+        localStorage.setItem("modalsState", JSON.stringify(state));
+      },
+    },
 })
 
-export const { setOpen } = openModalSlice.actions;
+export const { openModal, closeModal } = openModalSlice.actions;
 export default openModalSlice.reducer;
