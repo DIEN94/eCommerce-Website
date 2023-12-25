59b7e83f4f8c494a7b3e9cbeecbed8c957ae0bbb
DIEN94 <dm.kovalchyk94@gmail.com>
2023-12-25 18:38:26 +0200

    add like logic for CardOfProduct/Hover

--- src/components/CardOfProduct/components/Hover/config.ts
+++ src/components/CardOfProduct/components/Hover/config.ts
@@ -1,13 +1,11 @@
 import React, { FC, useState } from "react";
-import heart from 'assets/CardOfProduct/frames/Heart.webp'
 import compare from 'assets/CardOfProduct/frames/Compare.webp'
 import share from 'assets/CardOfProduct/frames/Share.webp'
 
 
-export type Keys = "share" | "compare" | "like"
+export type Keys = "share" | "compare"
 
 export const hoverConfig: Array<{key: Keys, src:string, text:string}> = [
     { key: "share",src: share, text:"Share"}, 
     { key: "compare",src: compare, text:"Compare"}, 
-    { key: "like", src: heart, text:"Like"}, 
 ]
