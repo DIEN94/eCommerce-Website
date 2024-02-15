b162c7d40d375e7b2167d8c94a5ef887c6ba86a2
DIEN94 <dm.kovalchyk94@gmail.com>
2024-02-15 22:26:00 +0200

    added auto scroll to top

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,31 +1,27 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { privateRoutes, publicRoutes } from 'router';
-import { useAppSelector } from 'hooks/redux';
-
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { privateRoutes, publicRoutes } from "router";
+import { useAppSelector } from "hooks/redux";
 
 export const AppRoutes = () => {
+  const { isAuth } = useAppSelector((state) => state.auth);
+  const { pathname } = useLocation();
 
-  const {isAuth} = useAppSelector(state => state.auth)
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-  return (
-    isAuth
-    ?
+  return isAuth ? (
     <Routes>
-        {privateRoutes.map(route =>
-        <Route 
-        {...route}
-        key={route.path}
-        />
-        )}
+      {privateRoutes.map((route) => (
+        <Route {...route} key={route.path} />
+      ))}
     </Routes>
-    :
+  ) : (
     <Routes>
-        {publicRoutes.map(route =>
-        <Route 
-        {...route}
-        key={route.path}
-        />
-        )}
+      {publicRoutes.map((route) => (
+        <Route {...route} key={route.path} />
+      ))}
     </Routes>
-    )}
+  );
+};
